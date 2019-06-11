a9b2b3c9bf23398e2cffd69dd9819d45d686f82c
Parker Agee <parker@parkeragee.com>
2019-06-11 14:53:59 -0500

    New pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -52,6 +52,8 @@ module.exports = function(grunt) {
                     'dist/open.html': './open.html',
                     'dist/react-developer-for-hire.html': './react-developer-for-hire.html',
                     'dist/netlify-hire-me-please.html': './netlify-hire-me-please.html',
+                    'dist/bradford-outline.html': './bradford-outline.html',
+                    'dist/akismet-process.html': './akismet-process.html',
                 }
             }
         },
